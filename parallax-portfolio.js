4107034b3d4d19a0eed4f52f19bf88e532efe2a8
Tim <tim.auldridge@outlook.com>
2020-09-30 13:29:10 -0700

    add template for code snippet

--- parallax-portfolio.js
+++ parallax-portfolio.js
@@ -6,3 +6,15 @@ $("button").click(function () {
     999
   );
 });
+
+$(".show-snippet").click(function () {
+  $(this).hide();
+  $(this).siblings(".hidden").show();
+  $(this).siblings(".snippet").css("display", "flex");
+});
+
+$(".hide-snippet").click(function () {
+  $(this).siblings(".show").show();
+  $(this).hide();
+  $(this).siblings(".snippet").css("display", "none");
+});
